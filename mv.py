82fde0e7fd253345110390700d9f9245d56e03b9
takeshi888 <how_about_yu@outlook.com>
2020-02-05 00:39:52 +0900

    fix reading file

--- mv.py
+++ mv.py
@@ -4,20 +4,21 @@ import os
 import shutil
 
 import numpy as np
-
+import pdb
 
 # Path ------------------------------------------------------------------------
 logsPath = "logs"
-dirPath = "b2b3b4b5b6400-450"
+dirPath = "b2b3b4b5b6205-300"
 mvPath = "tmp"
 # -----------------------------------------------------------------------------
 
 # Reading ---------------------------------------------------------------------
-pathFile = np.loadtxt("Top_1_path.txt").split("/")[-1]
+with open("Top_190_path.txt") as f:
+    pathFile = [line.strip().split("/")[-1] for line in f.readlines()]
 # -----------------------------------------------------------------------------
 
 # Move ------------------------------------------------------------------------    
 for file in pathFile:
-    if os.path.exists(file):
-        shutil.move(file,os.path.join(logsPath,mvPath,file))
+    if os.path.exists(os.path.join(logsPath,dirPath,file)):
+        shutil.move(os.path.join(logsPath,dirPath,file),os.path.join(logsPath,mvPath,file))
 # -----------------------------------------------------------------------------
