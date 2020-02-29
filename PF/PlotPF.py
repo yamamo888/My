76fd5a51fffbeeb7eec47098e9f6a2d3b33c7e58
yamamo888 <32571202+yamamo888@users.noreply.github.com>
2020-02-29 16:19:11 +0900

    🎨 : Change code

--- PF/PlotPF.py
+++ PF/PlotPF.py
@@ -7,98 +7,80 @@ import numpy as np
 import matplotlib.pylab as plt
 import matplotlib.pyplot as plt2
 import pylab
+import seaborn as sns
 
 import pdb
 
 # path ------------------------------------------------------------------------
 images = "images"
 numLinePath = "numlines"
+lhPath = "lhs"
 # -----------------------------------------------------------------------------
 
 
-# 数直線 ----------------------------------------------------------------------
-def NumberLine(x,pred,label="auto"):
+# 数直線 -----------------------------------------------------------------------
+def NumberLine(x,pred,time=0,label="auto"):
     """
     発生年数がどうなってるかを確認したくって
     [Args]
-        x: 真値t
-        pred: 予測値t(zero padding 済み)
+        x   : 真値t list[nk,tnk,tk]
+        pred: 予測値t
     """
-    
-    for p in np.arange(pred.shape[1]):
-        
-        xhat = pred[:,p]
+    for cell in np.arange(3):
+        #pdb.set_trace()
+        # predict year [perticles,]
+        xhat = pred[:,cell]
         # y = 0
         #y = [0] * x.shape[0]
         y = [0] * 1
         yhat = [0] * xhat.shape[0]
         
-        # 数直線 -------------------------------------------------------------------
+        # 数直線 -----------------------------------------------------------
         fig,ax=plt2.subplots(figsize=(10,10)) #画像サイズ
         fig.set_figheight(1) #高さ調整
         ax.tick_params(labelbottom=True, bottom=False) #x軸設定
         ax.tick_params(labelleft=False, left=False) #y軸設定
-        # -------------------------------------------------------------------------
+        # -----------------------------------------------------------------
         
-        # 数直線上の数値を表示 -------------------------------------------------------
-        # 真値
-        ax.annotate(f'{x}',
-                         xy=(x,y[0]),
-                         xytext=(10, 20),
-                         textcoords='offset points',
-                         arrowprops=dict(arrowstyle="->",connectionstyle="arc3,rad=.3")
-                        )
-        for i in np.arange(xhat.shape[0]):
-            # 予測値
-            ax.annotate(f'{xhat[i]}',
-                         xy=(xhat[i],yhat[i]),
-                         xytext=(10, 20),
-                         textcoords='offset points',
-                         arrowprops=dict(arrowstyle="->",connectionstyle="arc3,rad=.3")
-                        )
-        # -------------------------------------------------------------------------
-        
-        # 散布図 ------------------------------------------------------------------
+        # 散布図 -----------------------------------------------------------
         plt2.scatter(x,y[0],c='coral') # 真値
         plt2.scatter(xhat,yhat,c='skyblue') # 予測値
-        # -------------------------------------------------------------------------
+        # -----------------------------------------------------------------
        
-        # グラフの体裁----------------------------------------------------------------
+        # グラフの体裁--------------------------------------------------------
         xMin, xMax = np.min(np.append(x,xhat)), np.max(np.append(x,xhat))  
         plt2.tight_layout() #グラフの自動調整    
-        plt2.hlines(y=0,xmin=xMin,xmax=xMax) #横軸
+        plt2.hlines(y=0,xmin=xMin,xmax=xMax,color="silver") #横軸
         #plt2.xticks(np.arange(xMin,xMax,2)) #目盛り数値
         pylab.box(False) #枠を消す
-        # -------------------------------------------------------------------------
+        # -----------------------------------------------------------------
         
-        #plt2.show()
-        plt2.savefig(os.path.join(images,numLinePath,f"{label}_perticles{p}.png"))
+        plt2.savefig(os.path.join(images,numLinePath,f"{label}_{cell}.png"))
         plt2.close()
-        
 # -----------------------------------------------------------------------------
-    
 
-# 散布図 ----------------------------------------------------------------------
-def FeaturePlot(x):
-    """
-    V,Th,Bの推移を見たかった
-    """
+# ヒストグラム --------------------------------------------------------------------
+def HistLikelihood(weights,time=0,label="auto"):
     
-    for cell in np.arange(x[0].shape[1]):
-        # V
-        plt.scatter(x[0].shape[0],x[0][:,cell],color="blue")
-        plt.show()
-        plt.savefig(os.path.join(images,f"V_{cell}.png"))
-        plt.close()
-        # theta
-        plt.scatter(x[1].shape[0],x[1][:,cell],color="lightgreen")
-        plt.show()
-        plt.savefig(os.path.join(images,f"Theta_{cell}.png"))
-        plt.close()
-        # B
-        plt.scatter(x[2].shape[0],x[1][:,cell],color="pink")
-        plt.show()
-        plt.savefig(os.path.join(images,f"B_{cell}.png"))
-        plt.close()
+    # mean & var for label
+    lhMean = np.mean(weights,0)
+    lhVar = np.var(weights,0)
+    #pdb.set_trace()
+    # plot
+    sns.set_style("dark")
+    
+    sns.distplot(weights[:,0],label="nk",kde=False,rug=False)
+    sns.distplot(weights[:,1],label="tnk",kde=False,rug=False)
+    sns.distplot(weights[:,2],label="tk",kde=False,rug=False)
+    
+    plt.legend(loc="upper right")
+    plt.suptitle(f"mean:{lhMean}\n var:{lhVar}")
+    plt.savefig(os.path.join(images,lhPath,f"{label}.png"))
+    plt.close()
 # -----------------------------------------------------------------------------
-    
+
+        
+        
+        
+        
+        
