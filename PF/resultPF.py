5899b4bb74da655bda056035dbd2bf833d3dbd44
yamamo888 <32571202+yamamo888@users.noreply.github.com>
2020-04-21 01:13:58 +0900

    ✨ : Add new function

--- PF/resultPF.py
+++ PF/resultPF.py
@@ -16,10 +16,15 @@ import numpy as np
 import makingDataPF as myData
 import PlotPF as myPlot
 
+
+mode = int(sys.argv[1])
+
+
 # bool --------
-isLH = True
-isBVTh = True
-isLast = True # for notupdataPF
+isLH = False
+isBVTh = False
+isLast = True
+isnotupdateLast = False # for notupdataPF
 isAnima = False
 # -------------
 
@@ -28,7 +33,8 @@ logsPath = "logs"
 imgPath = "images"
 savetxtPath = "savetxt"
 paramPath = "parFile"
-outputPath = "190"
+firstPath = "nk"
+outputPath = f"190_{mode}"
 featuresPath = "nankairirekifeature"
 txtPath = "*txt"
 saveimgPath = "PF"
@@ -36,8 +42,6 @@ animaPath = "animaPF"
 batFile = "PyToCPF.bat"
 # ---------------
 
-filePath = os.path.join(paramPath,outputPath,txtPath)
-files = glob.glob(filePath)
 
 with open(os.path.join(featuresPath,"nankairireki.pkl"), "rb") as fp:
     nkfiles = pickle.load(fp)
@@ -58,33 +62,55 @@ nP = 501
 
 # likelihood hist plot --------------------------------------------------------  
 if isLH:
-    #lhfilePath = os.path.join(savetxtPath,"lh","lh_*")
-    #lhfiles = glob.glob(lhfilePath)
+    print("Start plot histgram.")
     
-    glhfilePath = os.path.join(savetxtPath,"lh","lh_g_*")
-    plhfilePath = os.path.join(savetxtPath,"lh","lh_p_*")
+    savedlhpath = os.path.join(imgPath,f"lh_{mode}")
+    lhPath = os.path.join(savetxtPath,f"lh_{mode}")
+    # weight path
+    wfilePath = os.path.join(lhPath,"w_*")
+    wfiles = glob.glob(wfilePath)
+    
+    for t,wfile in enumerate(wfiles):
+        weight = np.loadtxt(wfile)
+        
+        myPlot.HistLikelihood(weight,path=savedlhpath,label=f"w_{t}",color="black")
+    
+    # only eq.years ----    
+    lhfilePath = os.path.join(lhPath,"lh_*")
+    lhfiles = glob.glob(lhfilePath)
+
+    sumlhfilePath = os.path.join(lhPath,"sum_lh_*")
+    sumlhfiles = glob.glob(sumlhfilePath)
+    
+    for t,(lhfile,sumlhfile) in enumerate(zip(lhfiles,sumlhfiles)):
+        lh = np.loadtxt(lhfile)
+        sum_lh = np.loadtxt(sumlhfile)
+     
+        myPlot.HistLikelihood(lh[:,ntI],path=savedlhpath,label=f"nk_{t}",color="orange")
+        myPlot.HistLikelihood(lh[:,tntI],path=savedlhpath,label=f"tnk_{t}",color="forestgreen")
+        myPlot.HistLikelihood(lh[:,ttI],path=savedlhpath,label=f"tk_{t}",color="royalblue")
+        myPlot.HistLikelihood(sum_lh,path=savedlhpath,label=f"all_{t}")
+    """
+    # eq.years & eq.times mode=15 ----
+    # path
+    glhfilePath = os.path.join(lhPath,"lh_g_*")
+    plhfilePath = os.path.join(lhPath,"lh_p_*")
     glhfiles = glob.glob(glhfilePath)
     plhfiles = glob.glob(plhfilePath)
     
-    sumlhfilePath = os.path.join(savetxtPath,"lh","sum_lh_*")
-    sumlhfiles = glob.glob(sumlhfilePath)
+    sumglhfilePath = os.path.join(lhPath,"sum_lh_g_*")
+    sumglhfiles = glob.glob(sumglhfilePath)
+    sumplhfilePath = os.path.join(lhPath,"sum_lh_p_*")
+    sumplhfiles = glob.glob(sumplhfilePath)
     
-    print("Start plot histgram.")
-    """
-    for t,(lhfile,sumlhfile) in enumerate(zip(lhfiles,sumlhfiles)):
-        lh = np.loadtxt(lhfile)
-        myPlot.HistLikelihood(lh[:,ntI],label=f"nk_{t}",color="orange")
-        myPlot.HistLikelihood(lh[:,tntI],label=f"tnk_{t}",color="forestgreen")
-        myPlot.HistLikelihood(lh[:,ttI],label=f"tk_{t}",color="royalblue")
-        myPlot.HistLikelihood(sum_lh,label=f"all_{t}")
-    """ 
-    
-    for t,(glhfile,plhfile,sumlhfile) in enumerate(zip(glhfiles,plhfiles,sumlhfiles)):
+    for t,(glhfile,plhfile,sumglhfile,sumplhfile) in enumerate(zip(glhfiles,plhfiles,sumglhfiles,sumplhfiles)):
         #pdb.set_trace()
         glh = np.loadtxt(glhfile)
         plh = np.loadtxt(plhfile)
         
-        sum_lh = np.loadtxt(sumlhfile)
+        sum_glh = np.loadtxt(sumglhfile)
+        sum_plh = np.loadtxt(sumplhfile)
+        
         # for eq. year error of likelihood
         myPlot.HistLikelihood(glh[:,ntI],label=f"g_nk_{t}",color="orange")
         myPlot.HistLikelihood(glh[:,tntI],label=f"g_tnk_{t}",color="forestgreen")
@@ -95,7 +121,9 @@ if isLH:
         myPlot.HistLikelihood(plh[:,ttI],label=f"p_tk_{t}",color="royalblue")
         
         # nk + tnk + tk of likelihood
-        myPlot.HistLikelihood(sum_lh,label=f"all_{t}")
+        myPlot.HistLikelihood(sum_glh,label=f"all_g_{t}")
+        myPlot.HistLikelihood(sum_plh,label=f"all_p_{t}")
+    """
 # -----------------------------------------------------------------------------
 
 # b,V,theta Plot --------------------------------------------------------------
@@ -106,7 +134,7 @@ if isBVTh:
     Thetas = np.zeros([nP,nCell,iS])
     Vs = np.zeros([nP,nCell,iS])
     # only first b file
-    ffilePath = os.path.join(logsPath,"bzero",txtPath)
+    ffilePath = os.path.join(logsPath,firstPath,txtPath)
     ffiles = glob.glob(ffilePath)
     """
     # first b -----------------------------------------------------------------
@@ -114,7 +142,7 @@ if isBVTh:
     flag = False
     for fID in np.arange(len(ffiles)):
         file = os.path.basename(ffiles[fID])
-        _,_,_,tmpB = myData.loadABLV(logsPath,"bzero",file)
+        _,_,_,tmpB = myData.loadABLV(logsPath,firstPath,file)
         
         tmpB = np.concatenate((tmpB[2,np.newaxis],tmpB[4,np.newaxis],tmpB[5,np.newaxis]),0)
         
@@ -125,8 +153,8 @@ if isBVTh:
             firstB = np.vstack([firstB,tmpB])
     
     minB,maxB = np.min(firstB,0),np.max(firstB,0)
-    pdb.set_trace()
-    #myPlot.scatter3D(firstB[:,ntI],firstB[:,tntI],firstB[:,ttI],rangeP=[minB,maxB],path="PF",title="first B",label="B")
+    
+    myPlot.scatter3D(firstB[:,ntI],firstB[:,tntI],firstB[:,ttI],rangeP=[minB,maxB],path="PF",title="first B",label="B")
     # -------------------------------------------------------------------------
     """
     """
@@ -185,7 +213,7 @@ if isBVTh:
     """
     # -------------------------------------------------------------------------
     # reading B file
-    bfiles = glob.glob(os.path.join(savetxtPath,'B',txtPath))
+    bfiles = glob.glob(os.path.join(savetxtPath,f'B_{mode}',txtPath))
     
     for iS in np.arange(9):
         # [perticle,cell]
@@ -219,10 +247,14 @@ if isBVTh:
         maxBs = np.array([np.array(updateBs[ind]) for ind in maxBsind])
         # rate of max perticle
         maxrate = int(np.max(ratebs[maxBsind]))
-        #pdb.set_trace()
+        
         # save paramters (high rate of perticle)
-        np.savetxt(os.path.join(savetxtPath,'maxB',f'maxB_{iS+1}_{maxrate}.txt'),maxBs*1000000,fmt='%d',delimiter=',')
-        myPlot.scatter3D_heatmap(updateBs[:,ntI],updateBs[:,tntI],updateBs[:,ttI],ratebs,rangeP=[minB,maxB],path='PF',title=f'mean:{meanB}\n median:{medianB}',label=f'Bheatmap_{iS+1}_{numBs}')
+        maxbpath = os.path.join(savetxtPath,f'maxB_{mode}')
+        myData.isDirectory(maxbpath)
+        np.savetxt(os.path.join(maxbpath,f'maxB_{iS+1}_{maxrate}.txt'),maxBs*1000000,fmt='%d',delimiter=',')
+        # plot 3D heatmap scatter
+        s3hpath = os.path.join(imgPath,f'PF_{mode}')
+        myPlot.scatter3D_heatmap(updateBs[:,ntI],updateBs[:,tntI],updateBs[:,ttI],ratebs,rangeP=[minB,maxB],path=s3hpath,title=f'mean:{meanB}\n median:{medianB}',label=f'Bheatmap_{iS+1}_{numBs}')
     # -------------------------------------------------------------------------
     
 # -----------------------------------------------------------------------------
@@ -230,36 +262,17 @@ if isBVTh:
 # last paramter b -------------------------------------------------------------
 if isLast:
     """
-    1. there are no log file.
-    2. ParamFilePF.csv is written for 8 var.
-    3. output logs/190
+    In logs/190: only log_8*
     """
     print("Last research....")
-    """
-    # Start making log files --------------------------------------------------
-    lockPath = "Lock.txt"
-    lock = str(1)
-    with open(lockPath,"w") as fp:
-        fp.write(lock)
-    # --------------------
-
-    os.system(batFile)
-
-    sleepTime = 3
-    # lockファイル作成時は停止
-    while True:
-        time.sleep(sleepTime)
-        if os.path.exists(lockPath)==False:
-            break
-    # -------------------------------------------------------------------------    
-    """
+    
     # logs 8 var.
     lastlogsPath = os.path.join(logsPath,outputPath,txtPath)
     lastlogs = glob.glob(lastlogsPath)
     
     flag = False
     index = []
-    for iS in np.arange(nP):
+    for iS in np.arange(len(lastlogs)):
        file = os.path.basename(lastlogs[iS])
        print(file)
        # Num. of file for sort index
@@ -268,14 +281,16 @@ if isLast:
        U, th, V, B = myData.loadABLV(logsPath,outputPath,file)
        B = np.concatenate([B[2,np.newaxis],B[4,np.newaxis],B[5,np.newaxis]],0)
        stYear = int(U[0,1]) - 2000
-       deltaU, _, _, pJ_all = myData.convV2YearlyData(U,th,V,nYear=10000,cnt=1,stYear=stYear)
-       #pdb.set_trace()
+       deltaU, _, _, pJ_all = myData.convV2YearlyData(U,th,V,nYear=10000,cnt=1,stYear=stYear,isLast=True)
+       
        # predict eq.
        deltaU = np.concatenate([deltaU[:,2,np.newaxis],deltaU[:,4,np.newaxis],deltaU[:,5,np.newaxis]],1)
        pred = [pJ_all[ntI]-int(U[0,1]),pJ_all[tntI]-int(U[0,1]),pJ_all[ttI]-int(U[0,1])]
        gt = [np.where(gtV[:,ntI]>0)[0],np.where(gtV[:,tntI]>0)[0],np.where(gtV[:,ttI]>0)[0]]
+       
        # plot & mae eq. of predict & gt
-       maxSim = myPlot.Rireki(gt,pred,path="rireki",label=f"{iS}_{np.round(B[ntI],6)}_{np.round(B[tntI],6)}_{np.round(B[ttI],6)}",isResearch=True)
+       rpath = os.path.join(imgPath,f'rireki_{mode}')   
+       maxSim = myPlot.Rireki(gt,pred,path=rpath,label=f"{iS}_{np.round(B[ntI],6)}_{np.round(B[tntI],6)}_{np.round(B[ttI],6)}",isResearch=True)
 
        index = np.append(index,fID)
        if not flag:
@@ -289,10 +304,37 @@ if isLast:
     sortfID = index[sortInd]
     
     # save sort mae & index
-    np.savetxt(os.path.join(savetxtPath,'mae',f"maxSims.txt"),sort_maxSims,fmt=f"%d")
-    np.savetxt(os.path.join(savetxtPath,'mae',f"index.txt"),sortInd,fmt=f"%d")       
+    np.savetxt(os.path.join(savetxtPath,'mae',f"maxSims_{mode}.txt"),sort_maxSims,fmt=f"%d")
+    np.savetxt(os.path.join(savetxtPath,'mae',f"index_{mode}.txt"),sortInd,fmt=f"%d")       
 # -----------------------------------------------------------------------------
 
+# -----------------------------------------------------------------------------    
+if isnotupdateLast:
+
+    lastlogsPath = os.path.join(logsPath,outputPath,txtPath)
+    lastlogs = glob.glob(lastlogsPath)
+    
+    flag = False
+    index = []
+    for iS in np.arange(len(lastlogs)):
+       file = os.path.basename(lastlogs[iS])
+       print(file)
+       # Num. of file for sort index
+       fID = int(file.split("_")[-1].split(".")[0])
+       
+       U, th, V, B = myData.loadABLV(logsPath,outputPath,file)
+       B = np.concatenate([B[2,np.newaxis],B[4,np.newaxis],B[5,np.newaxis]],0)
+       
+       yU, yth, yV, pJ_all = myData.convV2YearlyData(U,th,V,nYear=10000,cnt=0)
+       yU, yth, yV, pJ_all, maxSim, sYear = myData.MSErrorNankai(gtV,yU,yth,yV,pJ_all,nCell=nCell)
+       pred = [pJ_all[ntI],pJ_all[tntI],pJ_all[ttI]]
+       gt = [np.where(gtV[:,ntI]>0)[0],np.where(gtV[:,tntI]>0)[0],np.where(gtV[:,ttI]>0)[0]]
+       
+       # plot & mae eq. of predict & gt
+       rpath = os.path.join(imgPath,'rireki')
+       maxSim = myPlot.Rireki(gt,pred,path=rpath,label=f"{iS}_{np.round(B[ntI],6)}_{np.round(B[tntI],6)}_{np.round(B[ttI],6)}",isResearch=True)
+# -----------------------------------------------------------------------------    
+       
 # animate image of param b ----------------------------------------------------
 if isAnima:
     gifPath = os.path.join(imgPath,animaPath,"*png")
