a46f25d708148aec110db239b6c7b51d26b6aea7
yamamo888 <32571202+yamamo888@users.noreply.github.com>
2020-04-20 11:41:55 +0900

    ✨ : Add function
    mse var.

--- PF/norm_likelihood.py
+++ PF/norm_likelihood.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import pdb
+
 import matplotlib.pylab as plt
 import numpy as np
 from scipy.stats import poisson
+
 
 
 # parameter -------------------------------------------------------------------
@@ -142,4 +145,217 @@ def norm_likelihood_alltimes(y,x):
     poissons = poisson.pmf(x_times,y_times)
     
     return poissons
+# -----------------------------------------------------------------------------
+
+# 尤度 + 最近傍 (地震発生年数)  --------------------------------
+def norm_likelihood_nearest(y,x,s2=100,standYs=0,time=0):
+    gauss,years = np.zeros(nCell+1),np.zeros(nCell) # for penalty
+
+    y_nk = np.array([standYs[ntI]])
+    y_tnk = np.array([standYs[tntI]])
+    y_tk = np.array([standYs[ttI]])
+         
+    # any eq.
+    if not y_nk[0] == 0:
+        # nearist index of gt year [1,]
+        bestInd = np.abs(np.asarray(x[ntI]) - y_nk).argmin()
+        bestX = x[ntI][bestInd]
+        # mse       
+        error_nk = -(y_nk-bestX)**2
+        
+        gauss[ntI] = error_nk
+        years[ntI] = bestX
+
+    if not y_tnk[0] == 0:
+        
+        bestInd = np.abs(np.asarray(x[tntI]) - y_tnk).argmin()
+        bestX = x[tntI][bestInd]
+        
+        error_tnk = -(y_tnk-bestX)**2
+        
+        gauss[tntI] = error_tnk
+        years[tntI] = bestX
+
+    if not y_tk[0] == 0:
+        
+        bestInd = np.abs(np.asarray(x[ttI]) - y_tk).argmin()
+        bestX = x[ttI][bestInd]
+        
+        error_tk = -(y_tk-bestX)**2
+        
+        gauss[ttI] = error_tk
+        years[ttI] = bestX
+    
+    #gauss_ = gauss[gauss != 0]
+    # sum of gauss, [1,]
+    #sumgauss = np.cumsum(1/gauss_)[-1]
+    sumgauss = np.cumsum(gauss)[-1]
+    
+    return gauss, sumgauss, years
+# -----------------------------------------------------------------------------
+
+# 尤度 + penalty 最近傍 (地震発生年数)  --------------------------------
+def norm_likelihood_nearest_penalty(y,x,s2=100,standYs=0,time=0):
+    gauss,years = np.zeros(nCell+1),np.zeros(nCell) # for penalty
+
+    y_nk = np.array([standYs[ntI]])
+    y_tnk = np.array([standYs[tntI]])
+    y_tk = np.array([standYs[ttI]])
+    
+    # not eq. in tonakai
+    if y_tk[0] == 0:
+        # ※同化年数±100年に地震があった場合はpenalty
+        penaltyInd = np.where((x[ttI]>y_tnk-safetyNum)&(x[ttI]<y_tnk+safetyNum))[0].tolist()
+        
+        if penaltyInd == []:
+            pass
+        else:
+            xpenalty = x[ttI][penaltyInd]
+            # nearist penalty year [1,]
+            penalty_tnk = np.max((y_tnk-xpenalty)**2)
+            # ペナルティ分引くため
+            gauss[-1] = -penalty_tnk
+        
+    # any eq.
+    if not y_nk[0] == 0:
+        # nearist index of gt year [1,]
+        bestInd = np.abs(np.asarray(x[ntI]) - y_nk).argmin()
+        bestX = x[ntI][bestInd]
+        # mse 
+        error_nk = -(y_nk-bestX)**2
+       
+        gauss[ntI] = error_nk
+        years[ntI] = bestX
+
+    if not y_tnk[0] == 0:
+        
+        bestInd = np.abs(np.asarray(x[tntI]) - y_tnk).argmin()
+        bestX = x[tntI][bestInd]
+        
+        error_tnk = -(y_tnk-bestX)**2
+        
+        gauss[tntI] = error_tnk
+        years[tntI] = bestX
+
+    if not y_tk[0] == 0:
+        
+        bestInd = np.abs(np.asarray(x[ttI]) - y_tk).argmin()
+        bestX = x[ttI][bestInd]
+        
+        error_tk = -(y_tk-bestX)**2
+        
+        gauss[ttI] = error_tk
+        years[ttI] = bestX
+    
+    #gauss_ = gauss[gauss != 0]
+    # sum of gauss, [1,]
+    #sumgauss = np.cumsum(1/gauss_)[-1]
+    sumgauss = np.cumsum(gauss)[-1]
+    
+    return gauss, sumgauss, years
+
+# -----------------------------------------------------------------------------
+
+# 尤度 + safety & penalty 最近傍 (地震発生年数)  --------------------------------
+def norm_likelihood_nearest_safetypenalty(y,x,s2=100,standYs=0,time=0):
+    gauss,years = np.zeros(nCell+1),np.zeros(nCell) # for penalty
+
+    y_nk = np.array([standYs[ntI]])
+    y_tnk = np.array([standYs[tntI]])
+    y_tk = np.array([standYs[ttI]])
+    
+    # not eq. in tonakai
+    if y_tk[0] == 0:
+        # ※同化年数±100年に地震があった場合はpenalty
+        penaltyInd = np.where((x[ttI]>y_tnk-safetyNum)&(x[ttI]<y_tnk+safetyNum))[0].tolist()
+        #pdb.set_trace()
+        if penaltyInd == []:
+            pass
+        else:
+            xpenalty = x[ttI][penaltyInd]
+            # nearist penalty year [1,]
+            penalty_tnk = np.max((y_tnk-xpenalty)**2)
+            # ペナルティ分引くため
+            gauss[-1] = -penalty_tnk
+        
+    # any eq.
+    if not y_nk[0] == 0:
+        # nearist index of gt year [1,]
+        bestInd = np.abs(np.asarray(x[ntI]) - y_nk).argmin()
+        # no.1
+        bestX = x[ntI][bestInd]
+        delbestX = x[ntI][x[ntI] != bestX]
+        # no.2
+        best2ind = np.abs(np.asanyarray(delbestX) - y_nk).argmin() 
+        best2X = delbestX[best2ind]
+        
+        # mse for no.1   
+        error_nk = -(y_nk-bestX)**2
+        # mae for no.2
+        penalty_nk = -(y_nk-best2X)**2
+    
+        if np.abs(best2X-y_nk) <= penaltyNum:
+            # error no.1 + no.2
+            gauss[ntI] = error_nk + penalty_nk
+        else:
+            # error only no.1
+            gauss[ntI] = error_nk
+            
+        years[ntI] = bestX
+        
+    if not y_tnk[0] == 0:
+        
+        bestInd = np.abs(np.asarray(x[tntI]) - y_tnk).argmin()
+        # no.1
+        bestX = x[tntI][bestInd]
+        delbestX = x[tntI][x[tntI] != bestX]
+        # no.2
+        best2ind = np.abs(np.asanyarray(delbestX) - y_tnk).argmin() 
+        best2X = delbestX[best2ind]
+        
+        # mse for no.1   
+        error_tnk = -(y_tnk-bestX)**2
+        # mae for no.2
+        penalty_tnk = -(y_tnk-best2X)**2
+    
+        if np.abs(best2X-y_tnk) <= penaltyNum:
+            # error no.1 + no.2
+            gauss[tntI] = error_tnk + penalty_tnk
+        else:
+            # error only no.1
+            gauss[tntI] = error_tnk
+        
+        years[tntI] = bestX
+        
+    if not y_tk[0] == 0:
+        
+        bestInd = np.abs(np.asarray(x[ttI]) - y_tk).argmin()
+        # no.1
+        bestX = x[ttI][bestInd]
+        delbestX = x[ttI][x[ttI] != bestX]
+        # no.2
+        best2ind = np.abs(np.asanyarray(delbestX) - y_tk).argmin() 
+        best2X = delbestX[best2ind]
+        
+        # mse for no.1   
+        error_tk = -(y_tk-bestX)**2
+        # mae for no.2
+        penalty_tk = -(y_tk-best2X)**2
+    
+        if np.abs(best2X-y_tk) <= penaltyNum:
+            # error no.1 + no.2
+            gauss[ttI] = error_tk + penalty_tk
+        else:
+            # error only no.1
+            gauss[ttI] = error_tk
+        
+        years[ttI] = bestX
+    
+    #gauss_ = gauss[gauss != 0]
+    # sum of gauss, [1,]
+    #sumgauss = np.cumsum(1/gauss_)[-1]
+    
+    sumgauss = np.cumsum(gauss)[-1]
+    #pdb.set_trace()
+    return gauss, sumgauss, years
 # -----------------------------------------------------------------------------
--- PF/simulatePF.py
+++ PF/simulatePF.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import norm_likelihood
 import PlotPF as myPlot
-
+import makingDataPF as myData
 
 # Num. of all param Th,V,b
 nParam = 3
@@ -16,9 +16,15 @@ ntI,tntI,ttI = 0,1,2
 thInd,vInd,bInd = 0,1,2
 gt_Year = 1400
 state_Year = 2000
+# -----------------------------------------------------------------------------
+
+# path ------------------------------------------------------------------------
 # save txt path
 savetxtPath = "savetxt"
-
+# save img path
+imgPath = "images"
+lhPath = "lhs"
+# -----------------------------------------------------------------------------
 
 # 逆関数 -----------------------------------------------------------------------
 def InvF(WC,idex,u):
@@ -40,7 +46,7 @@ def resampling(initU,weights,nP=0):
     thres = [1/nP*i+initU for i in range(nP)]
     wc = np.cumsum(weights)
     k = np.asanyarray([InvF(wc,idx,val) for val in thres])
-    pdb.set_trace()
+    #pdb.set_trace()
     return k
 # -----------------------------------------------------------------------------
 
@@ -58,7 +64,7 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
         x: 地震年数(1400年). [(eq.years zero padding),perticles]
         sy: start of assimilation for perticles.
     """
-    pdb.set_trace()
+    #pdb.set_trace()
     # 1. 初期化 ----------------------------------------------------------------
     # 状態ベクトル theta,v,year　※1セルの時おかしいかも
     ThVec = np.zeros((nP,nCell))
@@ -67,6 +73,9 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
     xResampled = np.zeros((nParam,nP,nCell))
     # all weight
     maxW = np.zeros((nP))
+    maxgW = np.zeros((nP))
+    maxpW = np.zeros((nP))
+    
     # weight for eq. year in each cell + penalty
     gw = np.zeros((nP,nCell+1))
     # weight for eq. times
@@ -100,12 +109,14 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
         # 2.c & 2.d 各粒子の尤度と重み -------------------------------------------
         standYs = [y[ntI][t],y[tntI][t],y[ttI][t]]
 
-        if mode == 3 or mode == 4 or mode == 5:
+        #if mode == 'sp' or mode == 'b_sp' or mode == 'b_sp_nl':
+        if mode == 4 or mode == 5:
+            # eq. year error
             weight, maxweight, years = norm_likelihood.norm_likelihood_safetypenalty(y,yhat,standYs=standYs,time=t)
             
             gw[i] = weight
             maxW[i] = maxweight
-            
+        # if mode == 'sp_time' or mode == 'b_sp_time_nl':
         if mode == 6 or mode == 7 or mode == 9 or mode == 10:
             gweight, gmaxweight, years = norm_likelihood.norm_likelihood_safetypenalty(y,yhat,standYs=standYs,time=t)
             pweight = norm_likelihood.norm_likelihood_times(y,yhat,standYs=standYs)
@@ -115,19 +126,56 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
             
             maxW[i] = gmaxweight + pweight
         
-        if mode == 8: # No.9-55
+        # if mode == 'sp_alltime'
+        if mode == 8:
             gweight, gmaxweight, years = norm_likelihood.norm_likelihood_safetypenalty(y,yhat,standYs=standYs,time=t)
+            # all eq. time
             pweight = norm_likelihood.norm_likelihood_alltimes(y,yhat)
         
             gw[i] = gweight
             pw[i] = pweight
             
             maxW[i] = gmaxweight + pweight
-            
-        if mode == 11 or mode == 12:
+        
+        # if mode == 'b_time'
+        if mode == 11:
             weight = norm_likelihood.norm_likelihood_times(y,yhat,standYs=standYs)
             maxW[i] = weight
             years = np.array(standYs)
+        
+        # nearist -----
+        # if mode == 'b_near'
+        if mode == 12:
+            weight, maxweight, years = norm_likelihood.norm_likelihood_nearest(y,yhat,standYs=standYs,time=t)
+        
+            gw[i] = weight
+            maxgW[i] = maxweight
+        
+        # if mode == 'b_p_near'
+        if mode == 13:
+            weight, maxweight, years = norm_likelihood.norm_likelihood_nearest_penalty(y,yhat,standYs=standYs,time=t)
+        
+            gw[i] = weight
+            maxgW[i] = maxweight
+        
+        # if mode == 'b_sp_near_scale'
+        if mode == 21:
+            weight, maxweight, years = norm_likelihood.norm_likelihood_nearest_safetypenalty(y,yhat,standYs=standYs,time=t)
+                 
+            gw[i] = weight
+            maxgW[i] = maxweight
+        
+        # if mode == 'b_sp_time_near'
+        if mode == 22:
+            gweight, gmaxweight, years = norm_likelihood.norm_likelihood_nearest_safetypenalty(y,yhat,standYs=standYs,time=t)
+            #pweight = norm_likelihood.norm_likelihood_times(y,yhat,standYs=standYs)
+            pweight = norm_likelihood.norm_likelihood_alltimes(y,yhat)
+        
+            gw[i] = gweight
+            pw[i] = pweight
+            
+            maxgW[i] = gmaxweight
+            maxpW[i] = pweight
             
         # ---------------------------------------------------------------------
         #pdb.set_trace()
@@ -151,43 +199,54 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
             yearInds = np.vstack([yearInds,years])
     #pdb.set_trace()
     # 規格化 -------------------------------------------------------------------
-    if mode == 3 or mode == 9 or mode == 10 or mode == 11 or mode == 12:
+    if mode == 3 or mode == 9 or mode == 10 or mode == 11:
         # [perticles,]
+        # maeやと最小年数誤差が一番小さくなってしまうから
         wNorm = maxW/np.sum(maxW)
-        
+      
+    # if mode == 'sp_alltime' or mode == 'sp_time' or mode == 'b_sp_nl' or mode == 'b_sp_time_nl':
     elif mode == 4 or mode == 5 or mode == 6 or mode == 7 or mode == 8:
         # scalling maximum(M),minimum(m)
         xmax = np.max(maxW)
         xmin = np.min(maxW)
         
-        m = 1/(np.sqrt(2*np.pi*100)) * np.exp(-((standYs[tntI]-gt_Year)/10)**2/(2*100))
+        #m = 1/(np.sqrt(2*np.pi*100)) * np.exp(-((standYs[tntI]-gt_Year)/10)**2/(2*100))
+        m1 = 1/(np.sqrt(2*np.pi*100)) * np.exp(-((standYs[tntI]-gt_Year)/10)**2/(2*100))
+        m2 = 1/(np.sqrt(2*np.pi*100)) * np.exp(-((standYs[tntI]-0)/10)**2/(2*100))
+        #pdb.set_trace()
+        m = np.min([m1,m2])
         M = xmax + m
         
         # normalization
         scaledW =  ((maxW - xmin)*(M - m) / (xmax - xmin)) + m
         
-        wNorm = scaledW/np.sum(scaledW)    
+        wNorm = scaledW/np.sum(scaledW)
+    
+    # nearist ----
+    # only eq.years
+    elif mode == 12 or mode == 13 or mode == 21:
+        # 全セルがぴったりの時
+        if any(maxgW==0):
+            zeroind = np.where(maxgW==0)[0].tolist()
+            maxgW[zeroind] = -1
+        
+        tmpgW = 1/-maxgW
+        wNorm = tmpgW/np.sum(tmpgW)
+        
+    # eq.years & eq.times
+    elif mode == 22:    
+        if any(maxgW==0):
+            zeroind = np.where(maxgW==0)[0].tolist()
+            maxgW[zeroind] = -1
+        
+        tmpgW = 1/-maxgW
+        
+        maxW = tmpgW + maxpW 
+        wNorm = maxW/np.sum(maxW)
+    
+    print(wNorm)
     # -------------------------------------------------------------------------
-
-    # save year & likelihood txt ----------------------------------------------
-    if isSavetxt:
-        """
-        if mode == 4 or mode == 5 or mode == 6 or mode == 7 or mode == 8:
-            np.savetxt(os.path.join(savetxtPath,"lh",f"sum_lh_{t}.txt"),scaledW,fmt="%4f")
-        else:    
-            np.savetxt(os.path.join(savetxtPath,"lh",f"sum_lh_{t}.txt"),maxW,fmt="%4f")
-        """
-        if mode == 6 or mode == 7 or mode == 8 or mode == 9 or mode == 10:            
-            np.savetxt(os.path.join(savetxtPath,"lh",f"lh_p_{t}.txt"),pw)
-            np.savetxt(os.path.join(savetxtPath,"lh",f"lh_g_{t}.txt"),gw)
-        elif mode == 11 or mode == 12:
-            np.savetxt(os.path.join(savetxtPath,"lh",f"sum_lh_{t}.txt"),maxW,fmt="%4f")
-        else:
-            np.savetxt(os.path.join(savetxtPath,"lh",f"lh_{t}.txt"),gw)
-            
-        np.savetxt(os.path.join(savetxtPath,"bestyear",f"by_{t}.txt"),yearInds,fmt="%d")
-    # -------------------------------------------------------------------------
-    pdb.set_trace()
+    #pdb.set_trace()
     # =========================================================================
     #         リサンプリング
     # =========================================================================
@@ -196,8 +255,8 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
     # ※3セル同じ組み合わせのbが選ばれる
     # index for resampling
     k = resampling(initU,wNorm,nP=nP)
-        
-    if mode == 3 or mode == 4 or mode == 7 or mode == 10 or mode == 12:
+    # if mode == 'sp' or mode == 'sp_time' or mode == 'sp_time_notscall'
+    if mode == 4 or mode == 7 or mode == 10:
         
         # system noise --------------------------------------------------------
         # ※元の値と足してもマイナスになるかも
@@ -215,7 +274,8 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
         
         updatesy = sy[k]
     
-    if mode == 5 or mode == 6 or mode == 8 or mode == 9 or mode == 11:
+    # if mode == 'sp_alltime' or mode == 'b_sp_nl' or mode == 'b_sp_time_nl':
+    if mode == 6 or mode == 8 or mode == 9 or mode == 11 or mode == 12 or mode == 13 or mode == 21 or mode == 22:
         #pdb.set_trace()
         # index for mean theta,V,b
         indmu = np.argmax(wNorm)
@@ -224,7 +284,7 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
         Bin = 10
         sigmaB = [[0,Bin,Bin],[Bin,0,Bin],[Bin,Bin,0]]
         
-        # 尤度の高いところにばらまく
+        # 尤度の1番高いところを中心に、次のbの分布決定
         # system noise --------------------------------------------------------
         # [cell,perticle]
         Thnoise = np.array([np.random.normal(0,0.01*np.mean(ThVec[:,cell]),nP) for cell in np.arange(nCell)])
@@ -236,20 +296,56 @@ def simulate(features,y,x,ssYears,mode=0,t=0,pTime=0,sy=0,nP=0,nCell=3,isSavetxt
         xResampled[thInd] = ThVec[k] + np.abs(Thnoise).T
         xResampled[vInd] = VVec[k] + np.abs(Vnoise).T
         xResampled[bInd] = np.abs(bnoise)*0.000001
-        xResampled[bInd][0] = features[bInd][indmu] # No.12.5
-        #xResampled[bInd] = features[bInd][k] + np.abs(bnoise)*0.000001
+        xResampled[bInd][0] = features[bInd][indmu]
         # 尤度が一番高いperticleの年数に合わせる
         updatesy = np.array(sy[indmu].tolist()*nP)[:,np.newaxis]
+    
     #pdb.set_trace()
     
     print(f"---- 【{t}】 times ----\n")
     # 発生年数 plot ------------------------------------------------------------
     if isPlot:
-        myPlot.NumberLine(standYs,yearInds,label=f"best_years_{t}")
+        nlpath = os.path.join(imgPath,f'numlines_{mode}')
+        myData.isDirectory(nlpath)
+        myPlot.NumberLine(standYs,yearInds,path=nlpath,label=f"best_years_{t}")
+    # -------------------------------------------------------------------------
+    
+    # save year & likelihood txt ----------------------------------------------
+    if isSavetxt:
+        """
+        # if mode == 'sp' or mode == 'b_sp' or mode == 'b_sp_nl'
+        if mode == 4 or mode == 5 or mode == 6 or mode == 7 or mode == 8:
+            np.savetxt(os.path.join(savetxtPath,"lh",f"sum_lh_{t}.txt"),scaledW,fmt="%4f")
+        else:    
+            np.savetxt(os.path.join(savetxtPath,"lh",f"sum_lh_{t}.txt"),maxW,fmt="%4f")
+        # if mode == 'sp_alltime' or mode == 'sp_time_np' or mode == 'b_sp_time_nl'
+        if mode == 7 or mode == 8 or mode == 9 or mode == 10:            
+            np.savetxt(os.path.join(savetxtPath,"lh",f"lh_p_{t}.txt"),pw)
+            np.savetxt(os.path.join(savetxtPath,"lh",f"lh_g_{t}.txt"),gw)
+        elif mode == 11:
+            np.savetxt(os.path.join(savetxtPath,"lh",f"sum_lh_{t}.txt"),maxW,fmt="%4f")
+        else:
+            np.savetxt(os.path.join(savetxtPath,"lh",f"lh_{t}.txt"),gw)
+        """
+        # nearist ----
+        lhpath = os.path.join(savetxtPath,f"lh_{mode}")
+        myData.isDirectory(lhpath)
+        if mode == 12 or mode == 13 or mode == 21:
+            np.savetxt(os.path.join(lhpath,f"lh_{t}.txt"),gw)
+            np.savetxt(os.path.join(lhpath,f"sum_lh_{t}.txt"),maxgW)
+        else:
+            np.savetxt(os.path.join(lhpath,f"lh_g_{t}.txt"),gw)
+            np.savetxt(os.path.join(lhpath,f"lh_p_{t}.txt"),pw)
+            np.savetxt(os.path.join(lhpath,f"sum_lh_g_{t}.txt"),maxgW)
+            np.savetxt(os.path.join(lhpath,f"sum_lh_p_{t}.txt"),maxpW)
         
-        if mode == 4 or mode == 5 or mode == 6 or mode == 7 or mode == 8 or mode == 9 or mode == 10 or mode == 11 or mode == 12:
-            xt = features[bInd][k]
-            np.savetxt(os.path.join(savetxtPath,'B',f'{t}.txt'),xt,fmt='%6f')
+        np.savetxt(os.path.join(lhpath,f"w_{t}.txt"),wNorm)
+        
+        # Save param b
+        xt = features[bInd][k]
+        bpath = os.path.join(savetxtPath,f'B_{mode}')
+        myData.isDirectory(bpath)
+        np.savetxt(os.path.join(bpath,f'{t}.txt'),xt,fmt='%6f')    
     # -------------------------------------------------------------------------
     
     return xResampled, yearInds.astype(int), updatesy, k
