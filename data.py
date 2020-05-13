56c31cc6347c1870207a5ef1f015e3aeba177ea3
yamamo888 <s206292@center.wakayama-u.ac.jp>
2020-05-13 18:10:27 +0900

    fix data.py

--- data.py
+++ data.py
@@ -1,89 +1,107 @@
 
 import os
 import glob
-import glob
 import pickle
+
+import numpy as np
 
 import pdb
 
 
-dataPath = 'data'
-pdeName = 'shrodinger'
-pklPath = '*pkl'
-
-
-class NankaiData:
-    def __init__(self,nCell=5,nClass=10,nWindow=10):
-        #----------------------------- paramters --------------------------------------
-		
-        # number of input cell
-        self.nCell = nCell
-        # number of class
-        self.nClass = nClass
-        # number of sliding window
-        self.nWindow = nWindow
-        # init batch count
-        self.batchCnt = 0
+class PDEData:
+    def __init__(self,pdeName='burgers'):
         
-        # -----------------------------------------------------------------------------
-
-        # ------------------------------- path ----------------------------------------
-        self.features = "features"
-        self.nankaipkls = "nankaipickles"
-        self.nankaifeature = "nankairirekiFFT"
-        self.nankairireki = "nankairireki.pkl"
-        self.rirekiFullPath = os.path.join(self.features,self.nankairireki)
-# loading train & test dataset ----
-def loadData(pdeName='burgers'):
-
-    if pdeName == 'shrodinger':
+        self.dataPath = 'data'
+        self.pklPath = '*pkl'
+        self.pdeName = pdeName
         
-        # x,t,uu dataset
-        xtuuPath = os.path.join(dataPath,pdeName,'x*')
-        with open(glob.glob(exactPath)[0],'rb') as fp:
-            Exact_u = pickle.load(fp)
-            Exact_v = pickle.load(fp)
-            Exact_h = pickle.load(fp)
+    # Loading train & test dataset ----
+    def loadData(self):
+    
+        if self.pdeName == 'shrodinger':
+            
+            # x,t,uu dataset
+            xtuuPath = os.path.join(self.dataPath,self.pdeName,'x*')
+            with open(glob.glob(xtuuPath)[0],'rb') as fp:
+                x = pickle.load(fp)
+                t = pickle.load(fp)
+            
+            # exact dataset
+            exactPath = os.path.join(self.dataPath,self.pdeName,'Exact*')
+            with open(glob.glob(exactPath)[0],'rb') as fp:
+                Exact_u = pickle.load(fp)
+                Exact_v = pickle.load(fp)
+                Exact_h = pickle.load(fp)
+            
+            #labelPath = os.path.join(self.dataPath,self.pdeName,'label*')
+            #with open(glob.glob(labelPath)[0],'rb') as fp:
+                #label_h = pickle.load(fp)
+            
+            # test index
+            teInd = [75,100,125]
+            
+            trxInd = np.ones(x.shape[0], dtype=bool)
+            trtInd = np.ones(t.shape[0], dtype=bool)
+            
+            trxInd[teInd] = False
+            trtInd[teInd] = False
+            
+            # train data x,t,h(x,t)
+            self.xtrain = x[trxInd]
+            self.ttrain = t[trtInd]
+            self.htrain = Exact_h[trxInd,trtInd]
+            #self.hlabeltrain = label_h
+            
+            # test data
+            self.xtest = x[teInd]
+            self.ttest = t[teInd]
+            self.htest = Exact_h[:,teInd]
+            pdb.set_trace()
+            # train X,Y test X,Y
+            return [self.xtrain,self.ttrain], [self.htrain], [self.xtest,self.ttest], [self.htest]
+    # ----
+    
+    # Make label data ----
+    def annotateY(self,num_cls=10):
         
-        # exact dataset for plot
-        exactPath = os.path.join(dataPath,pdeName,'Exact*')
-        with open(glob.glob(exactPath)[0],'rb') as fp:
-            Exact_u = pickle.load(fp)
-            Exact_v = pickle.load(fp)
-            Exact_h = pickle.load(fp)
-
-
-        # train X,Y test X,Y
-        return [x0,tb], [u0,v0], [x_star], [u_star,v_star,h_star,Exact_h]
-# ----
-
-# mini-batch
-def nextBatch(self,batchSize):
+        ycls = np.arange(np.min(self.htrain)-1, np.max(self.htrain)+1, num_cls)
         
-        sInd = batchSize * self.batchCnt
-        eInd = sInd + batchSize
-       
-        # [number of data, cell(=5,nankai2 & tonakai2 & tokai1), dimention of features(=10)]
-        trX = np.concatenate((self.x11Train[:,1:6,:],self.x12Train[:,1:6,:],self.x13Train[:,1:6,:]),0) 
-        # mini-batch, [number of data, cell(=5)*dimention of features(=10)]
-        batchX = np.reshape(trX[sInd:eInd],[-1,self.nCell*self.nWindow])
-        # test all targets
-        trY1 = np.concatenate((self.y11Train,self.y12Train,self.y13Train),0)
-        trY2 = np.concatenate((self.y31Train,self.y32Train,self.y33Train),0)
-        trY3 = np.concatenate((self.y51Train,self.y52Train,self.y53Train),0)
-        # [number of data(mini-batch), cell(=3)] 
-        batchY = np.concatenate((trY1[sInd:eInd,np.newaxis],trY2[sInd:eInd,np.newaxis],trY3[sInd:eInd,np.newaxis]),1)
+        flag = False
+        for data in self.htest:
+            pdb.set_trace()
+            oneHot = np.zeros(len(num_cls))
+            
+            for num in np.arange(num_cls+1):
+                if (data >= ycls[num]) & (data < ycls[num+1]):
+                    oneHot[num] = 1
+                
+            if not flag:
+                label = oneHot[np.newaxis]
+                flag = True
+            else:
+                label = np.vstack([label,oneHot])
+            
+            print(oneHot == 1)
+            
+        return label
+    # ----
+    
+    # mini-batch ----
+    def nextBatch(self,batchSize=100):
         
-        # train all labels, trlabel1 = nankai
-        trlabel1 = np.concatenate((self.y11TrainLabel,self.y12TrainLabel,self.y13TrainLabel),0)
-        trlabel2 = np.concatenate((self.y31TrainLabel,self.y32TrainLabel,self.y33TrainLabel),0)
-        trlabel3 = np.concatenate((self.y51TrainLabel,self.y52TrainLabel,self.y53TrainLabel),0)
-        # [number of data, number of class(self.nClass), cell(=3)] 
-        batchlabelY = np.concatenate((trlabel1[sInd:eInd,:,np.newaxis],trlabel2[sInd:eInd,:,np.newaxis],trlabel3[sInd:eInd,:,np.newaxis]),2)
-        #pdb.set_trace()
-        if eInd + batchSize > self.nTrain:
-            self.batchCnt = 0
-        else:
-            self.batchCnt += 1
-
+        nxTrain = self.xtrain.shape[0]
+        ntTrain = self.ttrain.shape[0]
+        
+        # random index for x & t
+        xInd = np.random.choice(nxTrain,batchSize)
+        tInd = np.random.choice(ntTrain,batchSize)
+            
+        xbatch = self.xtrain[xInd]
+        tbatch = self.ttrain[tInd]
+        # [x,t]
+        batchX = np.concatenate([xbatch[:,np.newaxis],tbatch[:,np.newaxis]],1)
+        batchY = self.htrain[xInd,tInd]
+        batchlabelY = self.hlabeltrain[xInd,tInd]
+        
         return batchX, batchY, batchlabelY
+    # ----
