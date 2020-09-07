9ab3f78d2d1d5801d65db24cbc2b14a0af29cb0a
yamamo888 <s206292@center.wakayama-u.ac.jp>
2020-09-07 13:38:33 +0900

    change plot

--- neuralnetworks/pdeplot.py
+++ neuralnetworks/pdeplot.py
@@ -49,16 +49,14 @@ class Plot:
       # ----
 
       # nu -> u(t,x) ----
-      def paramToU(self, params, xNum=256, tNum=100, savename='exact', isPlot=False):
+      def paramToU(self, params, xNum=256, tNum=100):
           
           x = params[0]
           t = params[1]
           nus = params[2]
-          exactnu = params[3]
           
           flag = False
-          losses = []
-          for enu, nu in zip(exactnu,nus):
+          for nu in nus:
               print(f'start param nu:{nu} -> inv u')
               # observation
               obsu = np.zeros([xNum, tNum])
@@ -75,49 +73,61 @@ class Plot:
     
                       obsu[i,j] = 4.0 - 2.0 * nu * dphi / phi
               
-              if isPlot:
-                  
-                  loss = np.square(enu - nu)
-                  losses = np.append(losses, loss)
-                  
-                  # plot u
-                  self.Uimg(x ,t, obsu, label=nu, savename=savename)
               # for pNN_burgers
+              if not flag:
+                  predobsu = obsu[np.newaxis]
+                  flag = True
               else:
-                  
-                  if not flag:
-                      predobsu = obsu[np.newaxis]
-                      flag = True
-                  else:
-                      predobsu = np.vstack([predobsu, obsu[np.newaxis]])
-        
-          if not isPlot:
-              return predobsu
-          else:
-              exactu_loss = np.hstack([exactnu, losses[:,np.newaxis]])
-              # save loss
-              np.savetxt(os.path.join('result',f'burgers_{savename}_{self.dataMode}.csv'), exactu_loss, fmt='%4f', delimiter=',')
-                  
-      # ----
+                  predobsu = np.vstack([predobsu, obsu[np.newaxis]])
 
+          return predobsu
+      # ----
+      
+      # ----
+      def plotExactPredParam(self, params, xNum=256, tNum=100, savename='test'):
+          
+          x = params[0]
+          t = params[1]
+          prednus = params[2]
+          exactnus = params[3]
+          
+          predus = self.paramToU([x,t,prednus], xNum=xNum, tNum=tNum)
+          exactus = self.paramToU([x,t,exactnus], xNum=xNum, tNum=tNum)
+          
+          for predu,exactu,prednu,exactnu in zip([predus,exactus,prednus,exactnus]):
+              # plot u
+              self.Uimg(x ,t, exactu, predu, label=f'{exactnu}_{prednu}', savename=savename)
+      # ----
+      
       # u(t,x) ----
-      def Uimg(self, x, t, u, label='test', savename='u'):
+      def Uimg(self, x, t, exactu, predu, label='test', savename='u'):
 
           X, T = np.meshgrid(x,t) #[100,256]
 
           X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None])) # [25600,2]
-          u_star = u.flatten()[:,None] # [25600,1]
-
-          # [100,256]
-          U_star = griddata(X_star, u_star.flatten(), (X, T), method='cubic')
-
+          
+          us = [exactu,predu]
           #pdb.set_trace()
+          fig, axes = plt.subplots(nrows=2,sharex="col")
+          for row,u in enumerate(us):
+              u_star = u.flatten()[:,None] # [25600,1]
+              # [100,256]
+              U_star = griddata(X_star, u_star.flatten(), (X, T), method='cubic')
 
-          img = plt.imshow(U_star.T, interpolation='nearest', cmap='gray',
-                           extent=[t.min(), t.max(), x.min(), x.max()],
-                           origin='lower', aspect='auto')
+              img = axes[row].imshow(U_star.T, interpolation='nearest', cmap='gray',
+                         extent=[t.min(), t.max(), x.min(), x.max()],
+                         origin='lower', aspect='auto')
 
-          #plt.colorbar()
+              divider1 = make_axes_locatable(axes[row])
+              cax1 = divider1.append_axes("right", size="2%", pad=0.1)
+              plt.colorbar(img,cax=cax1)
+              
+          #fig.canvas.draw()
+          #axpos1 = axes[0].get_position() # 上の図の描画領域
+          #axpos2 = ax2.get_position() # 下の図の描画領域
+          #幅をax1と同じにする
+          #ax2.set_position([axpos2.x0, axpos2.y0, axpos1.width, axpos2.height])
+              
           plt.tick_params(labelbottom=False,labelleft=False,labelright=False,labeltop=False)
           plt.tick_params(bottom=False,left=False,right=False,top=False)
           plt.axis('off')
